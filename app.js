0511d27704d53ab64a53c0b6e771ceb27cc3c877
Aman Thakur <amanthakur95@gmail.com>
2020-01-30 21:23:57 +0530

    Initialized react project

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-var $ = require('jquery');
-$('#app').text('Hello world')
+import React from 'react';
+import {render} from 'react-dom';
+
+const App = () => <h1>Hello from React</h1>
+
+render(<App />, document.getElementById('app'))
