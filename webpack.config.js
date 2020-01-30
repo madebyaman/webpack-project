ec9afaee481e6aa60b9267fbd31e336faee26d6c
Aman Thakur <amanthakur95@gmail.com>
2020-01-30 21:31:30 +0530

    Changed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,18 @@ module.exports = {
     filename: "bundle.js",
     publicPath: "/"
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {loader: 'babel-loader',
+          options: {
+          presets: ['@babel/preset-env',  '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
 }
